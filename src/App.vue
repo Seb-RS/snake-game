92eb4bc59ef190d1cf78c5eb04f5d218be9c5bb7
Sebastián Ríos Saavedra <sebadelhns@gmail.com>
2023-02-16 12:29:50 -0300

    feature: creating component and including it

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-
+    <gameBase></gameBase>
   </div>
 </template>
 
 <script>
+import gameBase from "./components/game-base.vue";
 export default {
-  
-}
+  components: {
+    gameBase,
+  },
+};
 </script>
---
+++ src/components/game-base.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <canvas ref="canvas" width="400" height="400"></canvas>
+    <p>Score:</p>
+    <button>Start Game</button>
+    <button>Stop Game</button>
+  </div>
+</template>
